62bccb265ab103a208a2625bce00afba44a93d7b
agustinT98 <agustoponi354@gmail.com>
2023-09-12 00:07:38 -0300

    form

--- src/form.php
+++ src/form.php
@@ -1,16 +1,30 @@
-<?php 
-    $nombre = $_POST['name'];
-    $mail = $_POST['email'];
-    $mensaje = $_POST['message'];
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
 
-    $mensaje = "Este mensaje fue enviado por" . $nombre . ". \r\n";
-    $mensaje .= "Su e-mail es: " . $mail . ". \r\n";
-    $mensaje .= "El mensaje es: " . $_POST['mensaje'] . ". \r\n";
+    // Configura los detalles del correo
+    $destinatario = "agustoponi354@gmail.com"; // Tu dirección de correo
+    $asuntoCorreo = "Nuevo mensaje de contacto desde el sitio web";
 
-    $to = 'agustoponi354@gmail.com';
-    $asunto = 'Contacto portfolio.';
+    // Construye el cuerpo del correo
+    $cuerpoCorreo = "Nombre: $nombre\n";
+    $cuerpoCorreo .= "Email: $email\n";
+    $cuerpoCorreo .= "Asunto: $asunto\n";
+    $cuerpoCorreo .= "Mensaje:\n$mensaje";
 
-    mail( $to, $asunto, ($mensaje), $header);
+    // Envía el correo
+    $resultado = mail($destinatario, $asuntoCorreo, $cuerpoCorreo);
 
-    $header('Location:index.html');
+    if ($resultado) {
+        // Redirige al usuario a una página de confirmación o agradecimiento
+        header("Location: index.html");
+        exit;
+    } else {
+        // Si hubo un error al enviar el correo, muestra un mensaje de error
+        echo "Error al enviar el correo. Por favor, inténtalo de nuevo más tarde.";
+    }
+}
 ?>
