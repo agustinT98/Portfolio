4a43328b67e50987175ac5f10b48d9284e5b15a7
agustinT98 <agustoponi354@gmail.com>
2023-09-19 12:07:19 -0300

    fix

--- src/form.php
+++ src/form.php
@@ -5,23 +5,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $asunto = $_POST["asunto"];
     $mensaje = $_POST["mensaje"];
 
-    // Configura los detalles del correo
+    
     $destinatario = "agustoponi354@gmail.com"; // Tu dirección de correo
     $asuntoCorreo = "Nuevo mensaje de contacto desde el sitio web";
 
-    // Construye el cuerpo del correo
+     
     $cuerpoCorreo = "Nombre: $nombre\n";
     $cuerpoCorreo .= "Email: $email\n";
     $cuerpoCorreo .= "Asunto: $asunto\n";
     $cuerpoCorreo .= "Mensaje:\n$mensaje";
 
-    // Envía el correo
+     
     $resultado = mail($destinatario, $asuntoCorreo, $cuerpoCorreo);
 
     if ($resultado) {
-        // Redirige al usuario a una página de confirmación o agradecimiento
-        header("Location: index.html");
-        exit;
+        if ($resultado) {
+            echo '<script>alert("Su mensaje ha sido enviado correctamente, muchas gracias!");</script>';
+            echo '<script>window.location = "index.html";</script>';
+            exit;
+        }
+    
     } else {
         // Si hubo un error al enviar el correo, muestra un mensaje de error
         echo "Error al enviar el correo. Por favor, inténtalo de nuevo más tarde.";
